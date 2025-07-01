af2f7747b454f61bb9f510b72081d4588969f17c
Nguyễn Tuấn Hùng <53892424+NgTHung@users.noreply.github.com>
2025-07-01 13:41:20 +0700

    [Add] method to get Ingredient from meal

--- src/Domain/Entity/Menu/Menu.cpp
+++ src/Domain/Entity/Menu/Menu.cpp
@@ -1,6 +1,25 @@
 #include "Menu.hpp"
 #include "../../../Core/Utils/IDManager.hpp"
 
+MealIngredient::MealIngredient(const std::string &name, const int quantity) : Name(name), Quantity(quantity) {
+    if (Name.empty()) {
+        throw std::invalid_argument("Ingredient name cannot be empty");
+    }
+    if (Quantity <= 0) {
+        throw std::invalid_argument("Ingredient quantity must be greater than zero");
+    }
+}
+
+MealIngredient::MealIngredient(const std::pair<std::string, int> &ingredient) : Name(ingredient.first),
+    Quantity(ingredient.second) {
+    if (Name.empty()) {
+        throw std::invalid_argument("Ingredient name cannot be empty");
+    }
+    if (Quantity <= 0) {
+        throw std::invalid_argument("Ingredient quantity must be greater than zero");
+    }
+}
+
 MenuAddon::MenuAddon(const std::string &Name, const double Price,
                      const std::vector<MealIngredient> &Ingredients) : MenuAddon(
     IDManager::GetInstance().GetNextID("MenuAddon"), Name, Price, Ingredients) {
