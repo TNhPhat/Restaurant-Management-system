9ea73f4ebd2a3a85ab0d99042a5cc26039347221
merdeput <nguyencaothong189@gmail.com>
2025-08-29 13:19:12 +0700

    [Add] DateTimePicker in Reservation

--- src/UI/Components/ReservationScreen.hpp
+++ src/UI/Components/ReservationScreen.hpp
@@ -4,6 +4,7 @@
 #include "../../Application/Reservation/ReservationManager.hpp"
 #include "../../Infrastructure/Reservation/FileReservationRepository.hpp"
 #include "../../Domain/Entity/Reservation/ReservationStatus.hpp"
+#include "DateTimePicker.hpp"
 
 #include <string>
 #include <unordered_map>
@@ -13,12 +14,17 @@ class ReservationScreen : public Screen {
 private:
     std::shared_ptr<ReservationManager> m_Manager;
 
+    // New reservation inputs
     std::vector<char> m_NewPhoneInput;
     std::string m_NewPhoneInputStr;
 
+    // Editing state
     std::unordered_map<std::string, int> m_EditedPeopleCount;
     std::unordered_map<std::string, DateTime> m_EditedDateTime;
+    std::unordered_map<std::string, DateTime> m_EditedReservationTime;
     std::unordered_map<std::string, ReservationStatus> m_EditedStatus;
+
+    DateTimePicker m_DatePicker;
 
 public:
     ReservationScreen(Core &core, std::shared_ptr<ReservationManager> manager);
@@ -31,11 +37,8 @@ public:
 
 private:
     void DrawNewReservationInput();
-
     void DrawReservationTable();
-
     void DrawSaveButton();
-
     void DrawBackButton();
 
     const char *StatusToString(ReservationStatus status);
